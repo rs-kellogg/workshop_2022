c20352ae998b66848a0d77b6bddad884137820ff
schorndorfer <will.k.t@gmail.com>
2022-07-19 13:13:57 -0500

    updating session 5 slides

--- Session3_Test/python/test_edgar.py
+++ Session3_Test/python/test_edgar.py
@@ -2,34 +2,28 @@ from pathlib import Path
 from edgar.forms.secdoc import Document
 
 
-def test_file1():
+def test_form3():
     # input data
-    file1 = Path("./data/11544_1_0000011544-20-000013.txt")
+    file = Path("form3.txt")
 
     # tested unit
     doc = Document(file1)
 
     # assertion
-    assert doc.accession_num == '0000011544-20-000013'
+    print(doc.accession_num)
+    print(doc.accession_num == '0000011544-20-000013')
 
 
-def test_file2():
+def test_file4():
     # input data
-    file2 = Path("./data/37996_4_0001209191-20-054135.txt")
+    file = Path("form4.txt")
 
     # tested unit
-    doc = Document(file2)
+    doc = Document(file)
 
     # assertion
-    assert False
+    # ADD CODE HERE
 
 
-def test_file3():
-    # input data
-    file3 = Path("./data/0001012975-17-000759.txt")
-
-    # tested unit
-    doc = Document(file3)
-
-    # assertion
-    assert False
+def test_file5():
+    # ADD CODE HERE
